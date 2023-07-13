1774151233e5a24d4b4738698ebf35b505b40818
Jasimraza Momin <jasimrazamomin@Jasimrazas-MacBook-Air.local>
2023-07-13 14:05:16 -0400

    added description of program

--- ATM_Bank_c.c
+++ ATM_Bank_c.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <math.h>
-
+/*
+The code works by first asking the user to input the number of accounts. I chose to do it by asking the user for how many
+accounts they'd like to set up first, then intializing the array. Then I'd ask for each accounts balance using a for loop.
+Finally, I'd ask the user for the interest rate as a decimal. Each part is surrounded by a while loop with an exit condition that
+can only be met if you enter a postive number or non-negative number. 
+Using a while loop and a switch statement, I print out the menu for the user to look at and choose an option from. The selection
+is stored in the choice variable and then the switch statement uses that for it's cases. 
+For the deposit option, the user is asked for the deposit amount, and if it is positive, the balance is changed, if not, an 
+error message is printed out.
+For the withdraw option, the user is asked for the withdraw amount. If it is negative or more money is being withdrawn than there
+is in the account, an error message is printed out.
+For the check balance option, the user's balance for their account number (stated at the start of the menu) is shown.
+For the future balance, the user is asked for the years the interest will accumulate and then calculate the final balance
+using the previously defined interest rate, balance, and years using a formula. If the user enters 0 or less years, they will
+get an error message.
+The account number is stored as 0-indexed to make accessing it easier, when it needs to be printed, a prefixed 1 is added to it
+to make it understandable for the user (i.e. account number 1 is stored as account choice 0, prints out as 1 + account choice).
+For the switch account option, the user is asked for which account they'd like to switch to. Using the account numbers asked for
+at the start and a while loop, it will switch their account selection to that account if it exists, if not, they will get an error
+message. 
+For the display all balances choice, a for loop is used to display all their accounts in numerical order.
+If the user decides to exit, an exit message is printed out. Incase of a misinput or a selection out of bounds, the default
+is used to catch it and print out an invalid selection message. 
+Used constants where needed, used floats for number that may be decimals, used integers for any whole numbers, and used a few 
+temp numbers in case the user entered an incorrect value, then made the constant values equal to that.
+*/
 int main()
 {
     const int account_nums; // make a constant int for accounts to make it unchangable and initialize rest of the variables
